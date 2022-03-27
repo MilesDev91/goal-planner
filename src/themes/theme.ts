c0661e1e717fe955225baf1e6fa93fe23591ccf7
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-03-27 09:49:00 -0400

    Project setup / scaffolding
    Setup project with packages and folders for desired work flow

---
+++ src/themes/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#e1e1e1",
+    },
+  },
+  typography: {
+    fontFamily: "'Roboto', 'Open Sans', 'sans-serif'",
+    fontSize: 12,
+  },
+});
