ed2f95718e3c3c5a57df58d1e27fb85cba761a42
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-03-27 10:36:39 -0400

    Navbar start and line ending settings

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Navbar: React.FC = () => {
+  return (
+    <Box>
+      <p>Hello</p>
+    </Box>
+  );
+};
+
+export { Navbar };
